bc253f3a24c1ecb389237a178af5395312ab1b23
Robert Sundström <robert.sundstrom@outlook.com>
2021-05-11 23:37:26 +0200

    Update frontend

---
+++ Frontend/HtmlExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+
+namespace Frontend
+{
+    public static class HtmlExtensions
+    {
+        public static string ReplaceMoreLink(this string str, string slug)
+        {
+            //Use the default configuration for AngleSharp
+            var config = Configuration.Default;
+
+            //Create a new context for evaluating webpages with the given config
+            var context = BrowsingContext.New(config);
+
+            var parser = context.GetService<IHtmlParser>();
+            var source = str;
+            var document = parser.ParseDocument(source);
+
+            var element = document.QuerySelector(".more-link");
+
+            if(element is AngleSharp.Html.Dom.IHtmlAnchorElement anchor)
+            {
+                anchor.Href = $"{slug}";
+            }
+
+            return document.ToHtml();
+        }
+    }
+}
