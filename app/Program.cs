a94b4c2e7e905406cc36e6f7b4f0eebb6936205d
Robert Sundström <robert.sundstrom@outlook.com>
2021-05-11 21:26:59 +0200

    Add Tye configuration

--- app/Program.cs
+++ app/Program.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using PeachPied.Demo.Plugins;
 using PeachPied.WordPress.AspNetCore;
 
@@ -19,38 +17,10 @@ namespace PeachPied.Demo
             //
             var host = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://*:5004/")
+                //.UseUrls("http://*:5004/")
                 .Build();
 
             host.Run();
-        }
-    }
-
-    class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddResponseCompression();
-            services.AddWordPress(options =>
-            {
-                // options.SiteUrl =
-                // options.HomeUrl = "http://localhost:5004";
-                
-                // options.PluginContainer.Add(new DashboardPlugin());
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IHostEnvironment env, IConfiguration configuration)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseWordPress();
-
-            app.UseDefaultFiles();
         }
     }
 }
